315c48d5dee0198183ea11058bb1596bad9dd532
Jason Field <jason@avon-lea.co.uk>
2020-01-04 20:39:00 +0000

    build

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const core = require('@actions/core');
 const exec = require('@actions/exec')
 
 
+const runCommand = async function (command){
+  exec.exec(command).catch(function(e) {
+    core.setFailed(e.message);
+  })
+}
+
 try {
   // Get the variables we care about
   const channel =- core.getInput('channel') || 'stable';
@@ -9,26 +15,15 @@ try {
   const version =- core.getInput('version');
   const omnitruckUrl =- core.getInput('omnitruckUrl') || 'omnitruck.chef.io';
   // Create the args that the bash script will need
-  var channelParam = `"-c ${channel}"`
-  var projectParam = `"-p ${project}"`
+  var channelParam = `-c ${channel}`
+  var projectParam = `-p ${project}`
   if (version) {
-    versionParam = `"-v ${version}"`
+    versionParam = `-v ${version}`
   }
   else {
     versionParam = ''
   }
-
-  // Ensure script is executable and run it
-  console.log('foo')
-  exec.exec('ls -la /home/runner/work/xor_test_cookbook').catch(function(e) {
-    core.setFailed(e.message);
-  })
-  exec.exec('chmod +x ./entrypoint.sh').catch(function(e) {
-      core.setFailed(e.message);
-    })
-  exec.exec(`./entrypoint.sh ${omnitruckUrl} ${channelParam} ${projectParam} ${versionParam}`).catch(function(e) {
-      core.setFailed(e.message);
-    })
+  runCommand(`script=$(curl -L https://${omnitruckUrl}/install.sh) && echo $script > chefDownload.sh && chmod +x ./chefDownload.sh && bash -s -- ${channelParam} ${projectParam} ${versionParam}`)
 
 } catch (error){
   core.setFailed(error.message);
