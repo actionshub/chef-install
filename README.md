30217a7e2e884805ff93d9b9d3a3dcf9e652d6ed
Dan Webb <dan.webb@damacus.io>
2026-02-18 22:33:40 +0000

    Pin chef-workstation to 21.6.497, update deps, and speed up Linux install
    - Default chef-workstation installs to 21.6.497 (last version with
      test-kitchen 2.x); later versions bundle test-kitchen 3.x which has
      breaking changes. Users can pass version: latest to opt in to newest.
    - Replace 4 sequential exec calls (curl/chmod/run/rm) with a single
      piped call, eliminating the temp file and reducing process spawns
    - Upgrade @actions/core and @actions/exec to v3.0.0; fixes two moderate
      undici CVEs in the transitive @actions/http-client dependency
    - Remove unnecessary 'os' npm package (Node.js built-in)
    - Fix var declarations to const/let

--- README.md
+++ README.md
@@ -45,15 +45,39 @@ jobs:
         project: inspec
 ```
 
+### Version pinning
+
+By default, `chef-workstation` installs version **21.6.497** — the last release that ships
+test-kitchen 2.x. Versions 21.7.524 and later bundle test-kitchen 3.x which contains
+breaking changes.
+
+To opt in to the latest release:
+
+```yaml
+    - name: install chef
+      uses: actionshub/chef-install@main
+      with:
+        version: latest
+```
+
+To pin to a specific version:
+
+```yaml
+    - name: install chef
+      uses: actionshub/chef-install@main
+      with:
+        version: 21.6.497
+```
+
 ## Parameters
 
 We support the following parameters
 
-| name         | default           | description                                                                            |
-| ------------ | ----------------- | -------------------------------------------------------------------------------------- |
-| channel      | stable            | Chef Channel to install, stable or current                                             |
-| project      | chef-workstation  | Which product to install, see <https://docs.chef.io/install_omnibus.html> for the list |
-| version      | latest            | version to install                                                                     |
-| omnitruckUrl | omnitruck.chef.io | which Omnitruck to use, default is Chef Official                                       |
+| name         | default                            | description                                                                            |
+| ------------ | ---------------------------------- | -------------------------------------------------------------------------------------- |
+| channel      | stable                             | Chef Channel to install, stable or current                                             |
+| project      | chef-workstation                   | Which product to install, see <https://docs.chef.io/install_omnibus.html> for the list |
+| version      | 21.6.497 (for chef-workstation)    | Version to install. Set to `latest` for the newest release.                            |
+| omnitruckUrl | omnitruck.chef.io                  | which Omnitruck to use, default is Chef Official                                       |
 
 By Changing the omnitruck Url you can also install Cinc projects
--- index.js
+++ index.js
@@ -1,51 +1,36 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec')
-const os = require('os')
+const exec = require('@actions/exec');
+const os = require('os');
 
 async function main() {
   try {
-    // Get the variables we care about
     const channel = core.getInput('channel') || 'stable';
     const project = core.getInput('project') || 'chef-workstation';
-    const version = core.getInput('version');
     const omnitruckUrl = core.getInput('omnitruckUrl') || 'omnitruck.chef.io';
-    // This tool has intimate knowledge of the os
-    // as Windows and Linux/MacOS run different installers
-    // so we will check what OS and run appropriately
-    // Create the args that the bash script will need
-    if (os.platform() != 'win32')
-    {
-      var channelParam = `-c ${channel}`
-      var projectParam = `-P ${project}`
-      if (version) {
-        versionParam = `-v ${version}`
-      }
-      else {
-        versionParam = ''
-      }
-      await exec.exec(`curl -L https://${omnitruckUrl}/install.sh -o chefDownload.sh`)
-      await exec.exec(`sudo chmod +x chefDownload.sh`)
-      await exec.exec(`sudo ./chefDownload.sh ${channelParam} ${projectParam} ${versionParam}`)
-      await exec.exec(`rm -f chefDownload.sh`)
-    }
-    // We are on windows so assume powershell
-    else
-    {
-      const windowsPath = core.getInput('windowsPath');
-      var channelParam = `-channel ${channel}`
-      var projectParam = `-project ${project}`
-      if (version) {
-        versionParam = `-version ${version}`
-      }
-      else {
-        versionParam = ''
-      }
-      await exec.exec(`powershell.exe -command ". { iwr -useb https://${omnitruckUrl}/install.ps1 } | iex; install ${channelParam} ${projectParam} ${versionParam}"`)
-      core.addPath(`${windowsPath}\\bin`)
+
+    // Default chef-workstation to the last version with test-kitchen 2.x.
+    // Later versions bundle test-kitchen 3.x which has breaking changes.
+    // Pass version: latest to opt in to the newest release.
+    let version = core.getInput('version');
+    if (!version && project === 'chef-workstation') {
+      version = '21.6.497';
     }
 
-  } catch (error){
+    if (os.platform() !== 'win32') {
+      const channelParam = `-c ${channel}`;
+      const projectParam = `-P ${project}`;
+      const versionParam = version ? `-v ${version}` : '';
+      await exec.exec('bash', ['-c', `set -o pipefail; curl -L "https://${omnitruckUrl}/install.sh" | sudo bash -s -- ${channelParam} ${projectParam} ${versionParam}`]);
+    } else {
+      const windowsPath = core.getInput('windowsPath');
+      const channelParam = `-channel ${channel}`;
+      const projectParam = `-project ${project}`;
+      const versionParam = version ? `-version ${version}` : '';
+      await exec.exec(`powershell.exe -command ". { iwr -useb https://${omnitruckUrl}/install.ps1 } | iex; install ${channelParam} ${projectParam} ${versionParam}"`);
+      core.addPath(`${windowsPath}\\bin`);
+    }
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
-main()
+main();
